b1bb4d002ee78d7b452dc5c41e439f230ebb51e9
minami murata <minami_murata@outlook.com>
2023-10-11 22:21:42 +0900

    7th day done.

--- react-guide-material/05_state_and_event/src/068_practice_obj_state/start/Example.js
+++ react-guide-material/05_state_and_event/src/068_practice_obj_state/start/Example.js
@@ -1,19 +1,29 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const Example = () => {
-  const orderObj = { item: 'apple', count: 10 };
+  const orderObj = { item: "apple", count: 10 };
   const [order, setOrder] = useState(orderObj);
-  const changeItem = (e) => {};
-  const countUp = () => {};
-  const countDown = () => {};
+  const changeItem = (e) => {
+    // setOrder({ ...order, item: e.target.value });
+    // 元の値を使う場合は、次行のようにコールバック関数で書くと良い
+    setOrder((order) => ({ ...order, item: e.target.value }));
+  };
+  const countUp = () => {
+    // setOrder({ ...order, count: order.count + 1 });
+    setOrder((order) => ({ ...order, count: order.count + 1 }));
+  };
+  const countDown = () => {
+    // setOrder({ ...order, count: order.count - 1 });
+    setOrder((order) => ({ ...order, count: order.count - 1 }));
+  };
   return (
     <div>
       <h3>練習問題</h3>
-      <p>
+      {/* <p>
         記述を変更し、完成コードのように「+と-ボタンをクリックするとCountの表示が1ずつ増減する機能」と「input要素に連動してItemの表示が変更される機能」を実装してください。コンポーネントの外側（上部）に変数や関数を準備しているためうまく使ってください。
-      </p>
-      <h3>Item:{/* ここにorder.itemを表示してください。*/}</h3>
-      <h3>Count:{/* ここにorder.countを表示してください。*/}</h3>
+      </p> */}
+      <h3>Item:{order.item}</h3>
+      <h3>Count:{order.count}</h3>
       <input type="text" value={order.item} onChange={changeItem} />
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
