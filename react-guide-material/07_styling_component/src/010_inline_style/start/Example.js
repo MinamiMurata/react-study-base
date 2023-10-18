f96cb8222605b279613f9f73d7eed529d224f9a7
minami murata <minami_murata@outlook.com>
2023-10-18 22:53:12 +0900

    10th day done.

--- react-guide-material/07_styling_component/src/010_inline_style/start/Example.js
+++ react-guide-material/07_styling_component/src/010_inline_style/start/Example.js
@@ -1,16 +1,32 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const Example = () => {
-    const [isSelected, setIsSelected] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
 
-    const clickHandler = () => setIsSelected(prev => !prev);
+  const clickHandler = () => setIsSelected((prev) => !prev);
 
-    return (
-        <>
-            <button onClick={clickHandler}>ボタン</button>
-            <div>{isSelected && "クリックされました。"}</div>
-        </>
-    )
+  const style = {
+    width: 120,
+    height: 60,
+    display: "block",
+    fontWeight: "bold",
+    "border-radius": 9999,
+    cursor: "pointer",
+    border: "none",
+    margin: "auto",
+    background: isSelected ? "pink" : "",
+    // インラインスタイルの場合は、擬似要素・擬似セレクタ（::beforeや:hoverなど）を使えない
+    // @media(min-width: などのメディアクエリ)の記述も不可
+    // パフォーマンスも良くない
+  };
+  return (
+    <>
+      <button onClick={clickHandler} style={style}>
+        ボタン
+      </button>
+      <div style={{ textAlign: "center" }}>{isSelected && "クリックされました。"}</div>
+    </>
+  );
 };
 
 export default Example;
--- react-guide-material/07_styling_component/src/045_practice_css_in_js/start/Example.js
+++ react-guide-material/07_styling_component/src/045_practice_css_in_js/start/Example.js
@@ -7,6 +7,16 @@ const FirstButton = styled.button`
   margin: 0.5rem 1rem;
   width: 11rem;
   border: none;
+  background-color: pink;
+`;
+
+const SecondButton = styled(FirstButton)`
+  background-color: red;
+  color: white;
+`;
+
+const ThirdButton = styled(SecondButton)`
+  background-color: ${(props) => (props.dark ? "black" : "green")};
 `;
 
 const Example = () => {
@@ -17,17 +27,16 @@ const Example = () => {
         記述を変更し、完成コードと同じ状態になるようにしてください。
         <ul>
           <li>Q1. FirstButtonのbackgroudをpinkにしてください。</li>
+          <li>Q2. FirstButtonを継承したSecondButtonを作成し、backgroudをredに、colorをwhiteにしてください。 </li>
           <li>
-            Q2.
-            FirstButtonを継承したSecondButtonを作成し、backgroudをredに、colorをwhiteにしてください。{" "}
-          </li>
-          <li>
-            Q3.
-            SecondButtonを継承したThirdButtonを作成し、props.darkがある場合のみbackgroudがblackに、ない場合はgreenになるようにしてください。
+            Q3. SecondButtonを継承したThirdButtonを作成し、props.darkがある場合のみbackgroudがblackに、ない場合はgreenになるようにしてください。
           </li>
         </ul>
       </p>
       <FirstButton>ボタン1</FirstButton>
+      <SecondButton>ボタン2</SecondButton>
+      <ThirdButton>ボタン3</ThirdButton>
+      <ThirdButton dark>ボタン3</ThirdButton>
     </>
   );
 };
