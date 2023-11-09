a034d24171ad5a489d39383ac3b2fe508cb42e38
minami murata <minami_murata@outlook.com>
2023-11-09 23:20:42 +0900

    15th day done.

--- react-guide-material/10_functional_programming/src/030_immutability/start/Example.js
+++ react-guide-material/10_functional_programming/src/030_immutability/start/Example.js
@@ -1,4 +1,13 @@
 const Example = () => {
+  const num = { val: 2 };
+  const doble = (num) => {
+    const newNum = { val: num.val * 2 };
+    // newNum.val = num.val * 2;
+    return newNum;
+  };
+  const newNum = doble(num);
+  console.log("newNum", newNum, "num", num);
+
   return (
     <>
       <h3>不変性（Immutability）</h3>
--- react-guide-material/11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
+++ react-guide-material/11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
@@ -1,7 +1,56 @@
+import { useState, useReducer } from "react";
+
 const Example = () => {
-  
+  const [state, setState] = useState(0);
+  const [rstate, dispatch] = useReducer((prev, { type, step }) => {
+    switch (type) {
+      case "+":
+        return prev + step;
+      case "-":
+        return prev - step;
+      default:
+        throw new Error("不明なactionです。");
+    }
+    // switch (action) {
+    //   case "+":
+    //     return ++prev;
+    //   case "-":
+    //     return --prev;
+    //   default:
+    //     throw new Error("不明なactionです。");
+    // }
+
+    // if (action === "+") {
+    //   return ++prev;
+    // } else if (action === "-") {
+    //   return --prev;
+    // }
+  }, 0);
+
+  const countUp = () => {
+    setState((prev) => ++prev);
+  };
+
+  const rcountUp = () => {
+    dispatch({ type: "+", step: 2 });
+  };
+  const rcountDown = () => {
+    dispatch({ type: "-", step: 3 });
+  };
+
   return (
     <>
+      <div>
+        <h3>State</h3>
+        <h3>{state}</h3>
+        <button onClick={countUp}>+</button>
+      </div>
+      <div>
+        <h3>Reducer</h3>
+        <h3>{rstate}</h3>
+        <button onClick={rcountUp}>+</button>
+        <button onClick={rcountDown}>-</button>
+      </div>
     </>
   );
 };
