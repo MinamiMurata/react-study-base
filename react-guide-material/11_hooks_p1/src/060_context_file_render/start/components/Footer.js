229a21947c1ae2a1d09eb86be4fdae1aec240fdb
minami murata <minami_murata@outlook.com>
2023-11-20 12:58:39 +0900

    16~17th day done.

---
+++ react-guide-material/11_hooks_p1/src/060_context_file_render/start/components/Footer.js
@@ -0,0 +1,16 @@
+// import { useUpdateTheme } from "../context/ThemeContext";
+// import { useTheme } from "../context/ThemeContext";
+
+const Footer = () => {
+  // const setTheme = useUpdateTheme();
+  // const [, setTheme] = useTheme();
+  console.log("footer");
+
+  return (
+    <footer>
+      <div>フッター</div>
+    </footer>
+  );
+};
+
+export default Footer;
--- react-guide-material/11_hooks_p1/src/060_context_file_render/start/components/Header.js
+++ react-guide-material/11_hooks_p1/src/060_context_file_render/start/components/Header.js
@@ -1,24 +1,21 @@
-import { useTheme } from "../context/ThemeContext"
-
+import { useTheme, useUpdateTheme } from "../context/ThemeContext";
 
 const Header = () => {
-  const [theme, setTheme] = useTheme();
+  const theme = useTheme();
+  const setTheme = useUpdateTheme();
 
   const THEMES = ["light", "dark", "red"];
 
   const changeTheme = (e) => setTheme(e.target.value);
+
+  console.log("header");
 
   return (
     <header className={`content-${theme}`}>
       {THEMES.map((_theme) => {
         return (
           <label key={_theme}>
-            <input
-              type="radio"
-              value={_theme}
-              checked={theme === _theme}
-              onChange={changeTheme}
-            />
+            <input type="radio" value={_theme} checked={theme === _theme} onChange={changeTheme} />
             {_theme}
           </label>
         );
