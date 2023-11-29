9049c339a5ab900e14c1c82cd2d0e80a009082ec
minami murata <minami_murata@outlook.com>
2023-11-29 22:23:17 +0900

    20th day done.

--- react-guide-material/12_hooks_p2/src/070_customHook/start/useTimer.js
+++ react-guide-material/12_hooks_p2/src/070_customHook/start/useTimer.js
@@ -1,45 +1,37 @@
 import { useEffect, useState, useLayoutEffect } from "react";
 
-const Example = () => {
-  const [isDisp, setIsDisp] = useState(true);
-
-  return (
-    <>
-      {isDisp && <Timer />}
-      <button onClick={() => setIsDisp((prev) => !prev)}>{isDisp ? "非表示" : "表示"}</button>
-    </>
-  );
-};
-
-const Timer = () => {
+const useTimer = () => {
   const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
-    console.log("init");
+    // console.log('init');
     let intervalId = null;
+
     if (isRunning) {
-      console.log("timer start");
+      // console.log('timer start');
+
       intervalId = window.setInterval(() => {
-        console.log("interval running");
+        // console.log('interval running');
         setTime((prev) => prev + 1);
       }, 1000);
     }
+
     return () => {
       window.clearInterval(intervalId);
-      console.log("end");
+      // console.log('end');
     };
   }, [isRunning]);
 
   useEffect(() => {
-    // console.log("updated");
+    // // console.log('updated');
 
     document.title = "counter:" + time;
     window.localStorage.setItem("time-key", time);
 
     return () => {
       // debugger
-      // console.log("updated end");
+      // // console.log('updated end');
     };
   }, [time]);
 
@@ -59,18 +51,12 @@ const Timer = () => {
     setIsRunning(false);
   };
 
-  return (
-    <>
-      <h3>
-        <time>{time}</time>
-        <span>秒経過</span>
-      </h3>
-      <div>
-        <button onClick={toggle}>{isRunning ? "一時停止" : "スタート"}</button>
-        <button onClick={reset}>リセット</button>
-      </div>
-    </>
-  );
+  return {
+    time,
+    isRunning,
+    toggle,
+    reset,
+  };
 };
 
-export default Example;
+export default useTimer;
